23ae7b35a9c2532d843286c59026a12449a55e44
YESSALMI <essalmiy06@gmail.com>
2022-06-03 17:26:10 +0100

    initial

---
+++ src/app.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+var push = require('web-push');
+
+const vapidKeys = {
+  publicKey: 'BBm3jZ7YLzphqamWgB7Z4xUJnoMdRYAwxSXX9e99Uvxy8gjsB1_nMD-QsoJPGyL5AyhkJyZmDYOuAWSV1-jkgPU',
+  privateKey: '7SvZqF6g49X6eW9pdsXSjD2aB6zHBdEQ82Q_kyN87ZY'
+}
+
+push.setVapidDetails('mailto:essalmiy06@gmail.com',vapidKeys.publicKey,vapidKeys.privateKey);
+let sub = {};
+push.sendNotificaiton(sub,"test Message")
+
+
+
+
+
+
+@Injectable()
+export class AppService { }
