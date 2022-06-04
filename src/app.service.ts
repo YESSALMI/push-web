97dfda59cb4f696e2b777036df4b15655d1ebda2
YESSALMI <essalmiy06@gmail.com>
2022-06-04 01:04:51 +0100

    second commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@nestjs/common';
-var push = require('web-push');
+// var push = require('web-push');
 
-const vapidKeys = {
-  publicKey: 'BBm3jZ7YLzphqamWgB7Z4xUJnoMdRYAwxSXX9e99Uvxy8gjsB1_nMD-QsoJPGyL5AyhkJyZmDYOuAWSV1-jkgPU',
-  privateKey: '7SvZqF6g49X6eW9pdsXSjD2aB6zHBdEQ82Q_kyN87ZY'
-}
+// const vapidKeys = {
+//   publicKey: 'BBm3jZ7YLzphqamWgB7Z4xUJnoMdRYAwxSXX9e99Uvxy8gjsB1_nMD-QsoJPGyL5AyhkJyZmDYOuAWSV1-jkgPU',
+//   privateKey: '7SvZqF6g49X6eW9pdsXSjD2aB6zHBdEQ82Q_kyN87ZY'
+// }
 
-push.setVapidDetails('mailto:essalmiy06@gmail.com',vapidKeys.publicKey,vapidKeys.privateKey);
-let sub = {};
-push.sendNotificaiton(sub,"test Message")
+// push.setVapidDetails('mailto:essalmiy06@gmail.com',vapidKeys.publicKey,vapidKeys.privateKey);
+// let sub = {
+//   endpoint: '.....',
+//   keys: {
+//     auth: '.....',
+//     p256dh: '56'
+//   }
+// };
+
+// push.sendNotification(sub, 'Your Push Payload Text');
 
 
 
